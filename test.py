e72b8817ecbe40540764644d68f9761e5536a155
hshar94 <hemantsharma@Hemants-MacBook-Air.local>
2021-05-27 11:36:53 +0530

    test

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from config import *
 
 s3 = boto3.resource('s3')
 
-try:
-    count = 0
-    for i in s3.Bucket(custombucket).objects.all():
-        count = count + 1
-    print(count)
+
+count = 0
+for i in s3.Bucket(custombucket).objects.all():
+    count = count + 1
+print(count)
